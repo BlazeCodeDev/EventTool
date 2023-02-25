46dcb2a922be35f646a249cbfc49971e1a715787
BlazeCodeDev <blazecodeapps@gmail.com>
2023-02-25 14:13:33 +0100

    Add venue and comments to search

--- app/src/main/java/com/blazecode/eventtool/database/EventDAO.kt
+++ app/src/main/java/com/blazecode/eventtool/database/EventDAO.kt
@@ -24,7 +24,14 @@ interface EventDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addEvent(vararg users: Event)
 
-    @Query("SELECT * FROM Event WHERE name LIKE :name OR firstName1 LIKE :name OR firstName2 LIKE :name OR lastName LIKE :name OR date LIKE :name")
+    @Query("SELECT * FROM Event " +
+            "WHERE name LIKE :name " +
+            "OR firstName1 LIKE :name " +
+            "OR firstName2 LIKE :name " +
+            "OR lastName LIKE :name " +
+            "OR date LIKE :name " +
+            "OR venue LIKE :name " +
+            "OR comments LIKE :name ")
     fun getEventsByName(name: String): MutableList<Event>
 
     @Query("DELETE FROM Event WHERE id LIKE :id")
