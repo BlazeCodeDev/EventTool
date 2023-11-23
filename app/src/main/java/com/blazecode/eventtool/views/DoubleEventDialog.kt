51cf4139bcdb8fd84bb619fb5dc16d5b3476c655
Ralf Lehmann <blazecodeapps@gmail.com>
2023-11-23 16:47:00 +0100

    Change from "multiple events found" to "choose event"

--- app/src/main/java/com/blazecode/eventtool/views/DoubleEventDialog.kt
+++ app/src/main/java/com/blazecode/eventtool/views/DoubleEventDialog.kt
@@ -1,26 +1,18 @@
 package com.blazecode.eventtool.views
 
 import android.content.Context
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.blazecode.eventtool.R
 import com.blazecode.eventtool.data.Event
 import com.blazecode.eventtool.enums.EventType
-import com.blazecode.eventtool.ui.theme.EventToolTheme
 
 @Composable
 fun DoubleEventDialog(context: Context, eventList: MutableList<Event>, onDismiss : () -> Unit, onClickEvent1 : () -> Unit, onClickEvent2 : () -> Unit) {
@@ -44,7 +36,7 @@ fun DoubleEventDialog(context: Context, eventList: MutableList<Event>, onDismiss
 
     AlertDialog(
         onDismissRequest = { onDismiss() },
-        title = { Text(stringResource(R.string.multiple_events_found)) },
+        title = { Text(stringResource(R.string.choose_event)) },
         text = {
             Column {
                 FilledTonalButton(
