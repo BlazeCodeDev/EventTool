517a268889610503a5473a48b919b07a58cc7e0e
Ralf Lehmann <blazecodeapps@gmail.com>
2023-11-25 16:08:49 +0100

    Release V1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.blazecode.eventtool"
         minSdk = 33
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.4.0"
+        versionCode = 20
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
