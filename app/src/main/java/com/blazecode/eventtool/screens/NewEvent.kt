a804a7d54031be9290e99c3a917dc8a5e11c37f5
Ralf Lehmann <blazecodeapps@gmail.com>
2024-02-03 13:17:55 +0100

    Fix deprecated code

--- app/src/main/java/com/blazecode/eventtool/screens/NewEvent.kt
+++ app/src/main/java/com/blazecode/eventtool/screens/NewEvent.kt
@@ -9,21 +9,60 @@ package com.blazecode.eventtool
 import android.content.Context
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -87,13 +126,13 @@ fun NewEvent(viewModel: NewEventViewModel = viewModel(), navController: NavContr
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun MainLayout(viewModel: NewEventViewModel, navController: NavController) {
     val context = LocalContext.current
 
     AnimatedContent(targetState = eventType.value,
-        transitionSpec = { fadeIn() with fadeOut() }) { targetState ->
+        transitionSpec = { fadeIn() togetherWith fadeOut() }, label = "pageFadeIn"
+    ) { targetState ->
         Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(0.dp,0.dp,0.dp, dimensionResource(R.dimen.fab_height_padding))){
             when (targetState) {
             EventType.WEDDING -> {
@@ -454,7 +493,7 @@ private fun CommentsLayout(viewModel: NewEventViewModel) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AdditionsLayout(viewModel: NewEventViewModel) {
-    val allAdditions: MutableList<Additions> = Additions.values().toMutableList()
+    val allAdditions: MutableList<Additions> = Additions.entries.toMutableList()
     val additions = rememberSaveable { mutableStateOf(viewModel.event.additions) }
 
     Card (modifier = Modifier
@@ -491,7 +530,7 @@ private fun AdditionsLayout(viewModel: NewEventViewModel) {
 }
 
 private fun getAddition(value: String): Additions? {
-    val map = Additions.values().associateBy(Additions::name)
+    val map = Additions.entries.associateBy(Additions::name)
     return map[value]
 }
 
